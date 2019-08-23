c4b32a6b3df7fc040515af85c7a418903e9c3b7c
Mafe Cordobes <mfcj307@gmail.com>
2019-08-22 19:28:35 -0500

    Add material ui to project

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,30 +1,10 @@
 import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import transformWeather from './../../services/transformWeather';
+import { api_weather } from './../../constants/api_url';
 import './styles.css';
-import {
-    SUN, WINDY,
-} from './../../constants/weathers';
 
-const location = "Buenos Aires,ar";
-const api_key = "7fdcc724256ed0a1196ae5ad6ed81a40";
-const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
-
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
-
- const data = {
-     temperature: 5, 
-     weatherState: SUN, 
-     humidity: 5, 
-     wind: '10 ms/s',
- }
-
- const data2 = {
-    temperature: 15, 
-    weatherState: WINDY, 
-    humidity: 12, 
-    wind: '20 ms/s',
-}
 
 //al extender de component vemos como podemos aprovechar el ciclo de vida de react
 //ejemplo manejar estados
@@ -34,36 +14,59 @@ class WeatherLocation extends Component {
         super(); //el constructor del componente base
         this.state = { //estado parcial del componente, asi para el const 
             city: "Buenos Aires",
-            data: data,
-        }
-    }
+            data: null,
+		}
+		console.log("constructor");
+	}
 
+	//aqui se ponen realmente las llamadas
+	componentDidMount() {
+		console.log("componentDidMount");
+		this.handleUpdateClick();
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log("componentDidUpdate");
+	}
+
+	componentWillMount() {
+		console.log("UNSAFEcomponentWillMount");
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		console.log("UNSAFEcomponentWillUpdate");
+	}
+	
     handleUpdateClick = () => {
         fetch(api_weather).then( resolve => {
             
             return resolve.json();
         }).then( data => {
-            console.log(data);
-            debugger;
-        });
-
-        console.log("atuzalizado");
+			const newWeather = transformWeather(data);
+			console.log(newWeather);
+			this.setState({ 
+				data: newWeather
+			});
         
-        this.setState({ //asi se meodifica , no se iguala mas 
-            data: data2,
         });
+        
+      
     }
 
 
     //this para cosas propias del componente 
 
     render() {
+		console.log("render");
         const { city, data } = this.state; // destructuring
         return (
             <div className="weatherLocationCont">
                 <Location city={city}></Location>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
+				{
+					data ? 
+					<WeatherData data={data}></WeatherData> : 
+					"Cargando..."
+				}
             </div>
         );
     }
